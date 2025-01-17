4c0e2def03c82b13ccee00f9c2fe89cc8d93035b
Manav <vermamanav117@gmail.com>
2025-01-17 15:07:59 +0530

    bugs fixes

--- src/Components/NewArrival/NewArrival.jsx
+++ src/Components/NewArrival/NewArrival.jsx
@@ -46,7 +46,7 @@ export const NewArrival = () => {
                         <div className={`col-12 d-inline-flex flex-column`} style={{
                             maxWidth: "100%",
                         }}>
-                            <h2 className={`${styles.brandInTitle} text-white text-center pb-4 col-12 ${windowWidth === "desktop" ? 'mb-4 fs-2' : 'mb-3 fs-3'} mt-0 fs-2`}>'✨ New Arrivals! ✨'</h2>
+                            <h2 className={`${styles.brandInTitle} text-white text-center pb-4 col-12 ${windowWidth === "desktop" ? 'mb-4 fs-2' : 'mb-3 fs-3'} mt-0 fs-2`}>✨ New Arrivals! ✨</h2>
                             <ReactOwlCarousel
                                 className={`${styles.brandSilder} brandSilder col-12 owl-theme`}
                                 margin={20}
